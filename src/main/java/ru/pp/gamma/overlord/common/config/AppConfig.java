5574b468d529630eee45c72893e8b0b044ac3978
yakovlev05 <alexey.yakovlev.05@mail.ru>
2026-04-07 23:31:02 +0500

    o-120 add xray support

--- src/main/java/ru/pp/gamma/overlord/common/config/AppConfig.java
+++ src/main/java/ru/pp/gamma/overlord/common/config/AppConfig.java
@@ -1,9 +1,12 @@
 package ru.pp.gamma.overlord.common.config;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import ru.pp.gamma.overlord.common.props.XrayProps;
 
 @Configuration
+@EnableConfigurationProperties(value = {XrayProps.class})
 @EnableWebSocket
 public class AppConfig {
 }
